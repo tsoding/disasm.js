1436abe508df81e0d6a926938cc053d2f7e683db
rexim <reximkut@gmail.com>
2019-12-24 05:38:23 +0700

    Ignore instructions that are marked with special ISAs

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
+"use strict";
+
 const xml2js = require('xml2js');
 const fs = require('fs');
 
 const opcodes = (async () => {
     let bytes = await fs.promises.readFile('./x86_64.xml', {});
-    return await xml2js.parseStringPromise(bytes.toString());
+    let allOpcodes = await xml2js.parseStringPromise(bytes.toString());
+    return allOpcodes.InstructionSet.Instruction;
 })();
 
-const refByNames = (async () =>
-    (await opcodes).InstructionSet.Instruction.reduce((result, x) => {
-        result[x.$.name] = x;
-        return result;
-    }, {})
-)();
+const refByNames = (async () => {
+    let result = {};
+    (await opcodes).forEach(opcode => result[opcode.$.name] = opcode);
+    return result;
+})();
 
 const refByOpcodes = (async () => {
     let result = {};
-    (await opcodes).InstructionSet.Instruction.forEach(
-        instr => instr.InstructionForm.forEach(
-            form => result[form.Encoding[0].Opcode.map(opcode => opcode.$.byte).join('')] = instr
+
+    (await opcodes).forEach(
+        opcode => opcode.InstructionForm.forEach(
+            form => {
+                // NOTE: ignore commands from "special" ISAs
+                if (typeof(form.ISA) === "undefined") {
+                    form.Encoding.forEach(
+                        enc => result[enc.Opcode.map(x => x.$.byte).join('')] = form
+                    );
+                }
+            }
         )
     );
+
     return result;
 })();
 
